8fee2f437c0ffad5ae3619e614f378e6b3bcbe70
bartoszmodelski <modelski.bartosz@gmail.com>
2023-01-11 10:56:22 +0100

    Reorganize tests (#16)
    * improve tests
    
    add tests on overlapping locations
    
    * add asserts
    
    * ensure test threads' executions actually overlap
    
    * remove mode
    
    * correct second test

--- test/test.ml
+++ test/test.ml
@@ -24,140 +24,153 @@
     OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
   ---------------------------------------------------------------------------*)
 
-open Kcas
-open Printf
+let nb_iter = 100_000
 
-let nb_iter = 10000
-let wait_time = 2
-let th1_success = Stdlib.ref true
-let th2_success = Stdlib.ref true
-let th3_success = Stdlib.ref true
-let th4_success = Stdlib.ref true
-let th5_success = Stdlib.ref true
-let v_x = 0
-let v_y = 1
+let assert_kcas ref expected_v =
+  let present_v = Kcas.get ref in
+  assert (present_v == expected_v)
 
-let thread1 (a1, a2) =
-  let c1 = [ mk_cas a1 v_x v_y; mk_cas a2 v_x v_y ] in
-  let c2 = [ mk_cas a1 v_y v_x; mk_cas a2 v_y v_x ] in
+module Barrier = struct
+  type t = { counter : int Atomic.t; total : int }
+
+  let make total = { counter = Atomic.make 0; total }
+
+  let await { counter; total } =
+    Atomic.incr counter;
+    while Atomic.get counter < total do
+      ()
+    done
+end
+
+(* test 1 *)
+let test_set () =
+  let a = Kcas.ref 0 in
+  assert_kcas a 0;
+  Kcas.set a 1;
+  assert_kcas a 1
+
+(* test 2 *)
+let thread1 barrier test_finished (a1, a2) () =
+  let c1 = [ Kcas.mk_cas a1 0 1; Kcas.mk_cas a2 0 1 ] in
+  let c2 = [ Kcas.mk_cas a1 1 0; Kcas.mk_cas a2 1 0 ] in
+
+  Barrier.await barrier;
+
   for _ = 1 to nb_iter do
-    let out1 = kCAS c1 in
-    let out2 = kCAS c2 in
-    if out1 <> true || out2 <> true then
-      (*      print_endline (sprintf "TH%d  APPEL N°%d ECHEC OUT1 = %b    OUT2 = %b!!!!!!!!!" (Domain.self ()) i out1 out2);*)
-      th1_success := false
+    assert_kcas a1 0;
+    assert_kcas a2 0;
+
+    let out1 = Kcas.kCAS c1 in
+    assert out1;
+
+    assert_kcas a1 1;
+    assert_kcas a2 1;
+
+    let out2 = Kcas.kCAS c2 in
+    assert out2
   done;
-  ignore @@ kCAS c1;
-  ()
+  Atomic.set test_finished true
 
-let thread2 (a1, a2) =
-  let c1 = [ mk_cas a1 v_y v_x; mk_cas a2 v_x v_y ] in
-  let c2 = [ mk_cas a1 v_x v_y; mk_cas a2 v_y v_x ] in
-  for _ = 1 to nb_iter do
-    let out1 = kCAS c1 in
-    let out2 = kCAS c2 in
-    if out1 <> false || out2 <> false then
-      (*      print_endline (sprintf "TH%d  APPEL N°%d ECHEC OUT1 = %b    OUT2 = %b!!!!!!!!!" (Domain.self ()) i out1 out2);*)
-      th2_success := false
+let thread2 barrier test_finished (a1, a2) () =
+  let c1 = [ Kcas.mk_cas a1 1 0; Kcas.mk_cas a2 0 1 ] in
+  let c2 = [ Kcas.mk_cas a1 0 1; Kcas.mk_cas a2 1 0 ] in
+
+  Barrier.await barrier;
+
+  while not (Atomic.get test_finished) do
+    let out1 = Kcas.kCAS c1 in
+    let out2 = Kcas.kCAS c2 in
+    assert (not out1);
+    assert (not out2)
   done
 
-let thread3 (a1, a2) =
-  let c1 = [ mk_cas a1 v_x v_y; mk_cas a2 v_y v_x ] in
-  let c2 = [ mk_cas a1 v_y v_x; mk_cas a2 v_x v_y ] in
-  for _ = 1 to nb_iter do
-    let out1 = kCAS c1 in
-    let out2 = kCAS c2 in
-    if out1 <> false || out2 <> false then
-      (*      print_endline (sprintf "TH%d  APPEL N°%d ECHEC OUT1 = %b    OUT2 = %b!!!!!!!!!" (Domain.self ()) i out1 out2);*)
-      th3_success := false
-  done
+let thread3 barrier test_finished (a1, a2) () =
+  let c1 = [ Kcas.mk_cas a1 0 1; Kcas.mk_cas a2 1 0 ] in
+  let c2 = [ Kcas.mk_cas a1 1 0; Kcas.mk_cas a2 0 1 ] in
 
-let thread4 (a1, a2) =
-  for i = 0 to nb_iter do
-    let c = [ mk_cas a1 i (i + 1); mk_cas a2 i (i + 1) ] in
-    let out = kCAS c in
-    if out <> true then th4_success := false
-  done
+  Barrier.await barrier;
 
-let thread5 (a1, a2) =
-  for _ = 0 to nb_iter do
-    let a = get a1 in
-    let b = get a2 in
-    if a > b then th5_success := false
+  while not (Atomic.get test_finished) do
+    let out1 = Kcas.kCAS c1 in
+    let out2 = Kcas.kCAS c2 in
+    assert (not out1);
+    assert (not out2)
   done
 
 let test_casn () =
-  let a1 = ref v_x in
-  let a2 = ref v_x in
+  let barrier = Barrier.make 3 in
+  let test_finished = Atomic.make false in
 
-  Domain.spawn (fun () -> thread1 (a1, a2)) |> ignore;
-  Domain.spawn (fun () -> thread2 (a1, a2)) |> ignore;
-  Domain.spawn (fun () -> thread3 (a1, a2)) |> ignore;
+  let a1 = Kcas.ref 0 in
+  let a2 = Kcas.ref 0 in
 
-  Unix.sleep wait_time;
-  print_endline (sprintf "a1 = %d et a2 = %d" (get a1) (get a2));
-  !th1_success && !th2_success && !th3_success
+  let domains = [ thread1; thread2; thread3 ] in
+  List.map (fun f -> Domain.spawn (f barrier test_finished (a1, a2))) domains
+  |> List.iter Domain.join
+
+(* test 3 *)
+
+let thread4 barrier test_finished (a1, a2) () =
+  Barrier.await barrier;
+  for i = 0 to nb_iter do
+    let c = [ Kcas.mk_cas a1 i (i + 1); Kcas.mk_cas a2 i (i + 1) ] in
+    assert (Kcas.kCAS c)
+  done;
+  Atomic.set test_finished true
+
+let thread5 barrier test_finished (a1, a2) () =
+  Barrier.await barrier;
+  while not (Atomic.get test_finished) do
+    let a = Kcas.get a1 in
+    let b = Kcas.get a2 in
+    assert (a <= b)
+  done
 
 let test_read_casn () =
-  let a1 = ref 0 in
-  let a2 = ref 0 in
+  let barrier = Barrier.make 2 in
+  let test_finished = Atomic.make false in
 
-  Domain.spawn (fun () -> thread4 (a1, a2)) |> ignore;
-  Domain.spawn (fun () -> thread5 (a1, a2)) |> ignore;
+  let a1 = Kcas.ref 0 in
+  let a2 = Kcas.ref 0 in
 
-  Unix.sleep wait_time;
-  !th4_success && !th5_success
+  let domains = [ thread4; thread5 ] in
+  List.map (fun f -> Domain.spawn (f barrier test_finished (a1, a2))) domains
+  |> List.iter Domain.join
 
-let test_set () =
-  let a = ref 0 in
-  let v1 = get a in
-  set a 1;
-  let v2 = get a in
-  print_endline (sprintf "V1 = %d et V2 = %d" v1 v2)
-
-let main_test () =
-  print_endline (sprintf "Test CASN");
-  if test_casn () then print_endline (sprintf "SUCCEEDED")
-  else print_endline (sprintf "FAILED");
-  print_endline (sprintf "Test READ CASN");
-  if test_read_casn () then print_endline (sprintf "SUCCEEDED")
-  else print_endline (sprintf "FAILED");
-  print_endline (sprintf "\nEND")
+(* test 4 *)
 
 let make_ref n =
-  let rec loop n out = if n > 0 then loop (n - 1) (ref 0 :: out) else out in
+  let rec loop n out =
+    if n > 0 then loop (n - 1) (Kcas.ref 0 :: out) else out
+  in
   loop n []
 
 let make_kcas0 r_l =
   let rec loop r_l out =
-    match r_l with h :: t -> loop t (mk_cas h 0 1 :: out) | [] -> out
+    match r_l with h :: t -> loop t (Kcas.mk_cas h 0 1 :: out) | [] -> out
   in
   loop r_l []
 
 let make_kcas1 r_l =
   let rec loop r_l out =
-    match r_l with h :: t -> loop t (mk_cas h 1 0 :: out) | [] -> out
+    match r_l with h :: t -> loop t (Kcas.mk_cas h 1 0 :: out) | [] -> out
   in
   loop r_l []
 
-let test_benchmark n nb_loop =
+let test_stress n nb_loop =
   let r_l = make_ref n in
   let kcas0 = make_kcas0 r_l in
   let kcas1 = make_kcas1 r_l in
-  let rec loop nb_loop =
-    if nb_loop > 0 then
-      if kCAS kcas0 then
-        if kCAS kcas1 then loop (nb_loop - 1)
-        else print_endline (sprintf "FAILURE KCAS1 n°%d" nb_loop)
-      else print_endline (sprintf "FAILURE KCAS0 n°%d" nb_loop)
-  in
-  loop nb_loop;
-  print_endline "TEST SUCCEED"
+  for _ = 1 to nb_loop do
+    assert (Kcas.kCAS kcas0);
+    assert (Kcas.kCAS kcas1)
+  done
 
 let () =
-  main_test ();
   test_set ();
-  test_benchmark 1000 10000
+  test_casn ();
+  test_read_casn ();
+  test_stress 1000 10000
 
 (*
 ####
