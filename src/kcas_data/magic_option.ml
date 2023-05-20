4eb2d245ee027def72091b493847587ab3ef9a0d
Vesa Karvonen <vesa.a.j.k@gmail.com>
2023-05-20 13:19:54 +0300

    Add `Mvar`

--- src/kcas_data/magic_option.ml
+++ src/kcas_data/magic_option.ml
@@ -1,3 +1,5 @@
+open Kcas
+
 type 'a t = 'a
 
 let none = ref ()
@@ -7,8 +9,11 @@ external some : 'a -> 'a t = "%identity"
 
 let is_none x = x == none [@@inline]
 let is_some x = x != none [@@inline]
-let get_or_retry x = if is_none x then Kcas.Retry.later () else x
+let get_or_retry x = if is_none x then Retry.later () else x [@@inline]
+let put_or_retry v x = if is_none x then some v else Retry.later () [@@inline]
+let take_or_retry x = if is_none x then Retry.later () else none [@@inline]
 
 external get_unsafe : 'a t -> 'a = "%identity"
 
-let to_option x = if is_none x then None else Some x
+let to_option x = if is_none x then None else Some x [@@inline]
+let of_option = function None -> none | Some x -> some x [@@inline]
