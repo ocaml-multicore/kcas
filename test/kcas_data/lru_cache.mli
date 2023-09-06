899482bfa6a17ed43cef779064573d6293f14558
Vesa Karvonen <vesa.a.j.k@gmail.com>
2023-09-05 20:00:15 -0700

    LRU-cache example

---
+++ test/kcas_data/lru_cache.mli
@@ -0,0 +1,18 @@
+open Kcas
+open Kcas_data
+
+type ('k, 'v) t
+
+val create : ?hashed_type:'k Hashtbl.hashed_type -> int -> ('k, 'v) t
+
+module Xt :
+  Lru_cache_intf.Ops
+    with type ('k, 'v) t := ('k, 'v) t
+    with type ('x, 'fn) fn := xt:'x Xt.t -> 'fn
+    with type ('x, 'fn) blocking_fn := xt:'x Xt.t -> 'fn
+
+include
+  Lru_cache_intf.Ops
+    with type ('k, 'v) t := ('k, 'v) t
+    with type ('x, 'fn) fn := 'fn
+    with type ('x, 'fn) blocking_fn := ?timeoutf:float -> 'fn
