2b4a1bedd9ef0182b295e23987c91686f268672e
Vesa Karvonen <vesa.a.j.k@gmail.com>
2023-12-29 20:10:18 +0200

    Add read-only `Xt` benchmark and access locs in optimal order

--- bench/main.ml
+++ bench/main.ml
@@ -3,6 +3,7 @@ let benchmarks =
     ("Atomic", Bench_atomic.run_suite);
     ("Kcas Loc", Bench_loc.run_suite);
     ("Kcas Xt", Bench_xt.run_suite);
+    ("Kcas Xt read-only", Bench_xt_ro.run_suite);
     ("Kcas parallel CMP", Bench_parallel_cmp.run_suite);
     ("Kcas_data Accumulator", Bench_accumulator.run_suite);
     ("Kcas_data Hashtbl", Bench_hashtbl.run_suite);
