f604aa15608401cdb769dbed769ff5c49e275f1b
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-12-19 12:25:47 +0200

    Use separate CAS and CMP lists

--- doc/simplified.ml
+++ doc/simplified.ml
@@ -4,34 +4,38 @@ module Kcas : sig
   val make : 'a -> 'a loc
   val get : 'a loc -> 'a
 
-  type cas = CAS : 'a loc * 'a * 'a -> cas | CMP : 'a loc * 'a -> cas
+  type cas = CAS : 'a loc * 'a * 'a -> cas
+  type cmp = CMP : 'a loc * 'a -> cmp
 
-  val atomically : cas list -> bool
+  val atomically : cas list -> cmp list -> bool
 end = struct
   type 'a loc = 'a state Atomic.t
   and 'a state = { before : 'a; after : 'a; casn : casn }
   and cass = CASS : 'a loc * 'a state -> cass
   and casn = status Atomic.t
-  and status = Undetermined of cass list | After | Before
+
+  and status =
+    | Undetermined of { cass : cass list; cmps : cass list }
+    | After
+    | Before
 
   let make after =
     Atomic.make { before = after; after; casn = Atomic.make After }
 
-  type cas = CAS : 'a loc * 'a * 'a -> cas | CMP : 'a loc * 'a -> cas
-
-  let is_cmp casn state = state.casn != casn
+  type cas = CAS : 'a loc * 'a * 'a -> cas
+  type cmp = CMP : 'a loc * 'a -> cmp
 
   let finish casn desired =
     match Atomic.get casn with
     | After -> true
     | Before -> false
-    | Undetermined cass as current ->
+    | Undetermined { cmps; _ } as current ->
         let desired =
           if
             desired == After
-            && cass
+            && cmps
                |> List.exists @@ fun (CASS (loc, state)) ->
-                  is_cmp casn state && Atomic.get loc != state
+                  Atomic.get loc != state
           then Before
           else desired
         in
@@ -43,11 +47,8 @@ end = struct
     | CASS (loc, desired) :: continue as retry -> begin
         let current = Atomic.get loc in
         if desired == current then gkmz casn continue
-        else if is_cmp casn desired then finish casn Before
         else
-          let current_value =
-            if is_after current.casn then current.after else current.before
-          in
+          let current_value = get_from current in
           if current_value != desired.before then finish casn Before
           else
             match Atomic.get casn with
@@ -59,34 +60,34 @@ end = struct
             | Before -> false
       end
 
-  and is_after casn =
-    match Atomic.get casn with
-    | Undetermined cass -> gkmz casn cass
-    | After -> true
-    | Before -> false
+  and get_from : 'a. 'a state -> 'a =
+   fun state ->
+    match Atomic.get state.casn with
+    | Undetermined { cass; _ } ->
+        if gkmz state.casn cass then state.after else state.before
+    | After -> state.after
+    | Before -> state.before
 
-  let get loc =
-    let state = Atomic.get loc in
-    if is_after state.casn then state.after else state.before
-
-  let atomically logical_cas_list =
+  let atomically logical_cas_list logical_cmp_list =
     let casn = Atomic.make After in
     let cass =
       logical_cas_list
       |> List.map @@ function
          | CAS (loc, before, after) -> CASS (loc, { before; after; casn })
+    in
+    let cmps =
+      logical_cmp_list
+      |> List.map @@ function
          | CMP (loc, expected) ->
              let current = Atomic.get loc in
-             if get loc != expected || Atomic.get loc != current then raise Exit
+             if get_from current != expected then raise Exit
              else CASS (loc, current)
     in
-    Atomic.set casn (Undetermined cass);
+    Atomic.set casn (Undetermined { cass; cmps });
     gkmz casn cass
 
-  let atomically logical_cas_list =
-    try atomically logical_cas_list with Exit -> false
+  let atomically logical_cas_list logical_cmp_list =
+    try atomically logical_cas_list logical_cmp_list with Exit -> false
 
-  let get loc =
-    let state = Atomic.get loc in
-    if is_after state.casn then state.after else state.before
+  let get loc = get_from (Atomic.get loc)
 end
